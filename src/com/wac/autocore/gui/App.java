36437d98cabb49b9d412cee36fea1c9f556fadb9
William Skoglund <w.skoglund00@gmail.com>
2026-09-20 11:07:15 +0200

    Changed stage title to english

--- src/com/wac/autocore/gui/App.java
+++ src/com/wac/autocore/gui/App.java
@@ -19,7 +19,7 @@ public class App extends Application {
         URL fxmlLocation = App.class.getResource("/com/wac/autocore/gui/view/MainView.fxml");
 
         if (fxmlLocation == null) {
-            throw new IllegalArgumentException("Hittade inte FXML-filen under resources!");
+            throw new IllegalArgumentException("Could not find the FXML file in the application resources!");
         }
 
         Parent root = FXMLLoader.load(fxmlLocation);
@@ -37,7 +37,7 @@ public class App extends Application {
 
         scene.getStylesheets().add(getClass().getResource("/com/wac/autocore/gui/css/styleguide.css").toExternalForm());
         //scene.getStylesheets().add(getClass().getResource("styleguide.css").toExternalForm());
-        stage.setTitle("Wigell Auto Core - Verkstadssystem");
+        stage.setTitle("Wigell Auto Core - Workshop Management System");
         stage.setScene(scene);
         stage.show();
         root.requestFocus();
